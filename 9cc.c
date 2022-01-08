7ecc354106e0094ddcee5f294c848e8bbcad3e9d
aoifukino <fukino1115@gmail.com>
2022-01-08 16:59:05 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,32 @@ int main(int argc,char **argv){
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n",atoi(argv[1]));
-	printf("  ret\n");
+	//strtol:文字列を整数に変換 数値を読み込んだ後、第2引数のポインタを
+	//アップデートして、次の文字を指すように値を更新する。
+	printf("	mov rax, %ld\n",strtol(p,&p,10));
+
+	while(*p){
+		if(*p == '+'){
+			p++;//+ → 20
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if(*p == '-'){
+			p++;//- →　4
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr,"予期しない文字です: '%c'\n",*p);
+		return 1;
+	}
+
+	printf("	ret\n");
 	return 0;
 }
